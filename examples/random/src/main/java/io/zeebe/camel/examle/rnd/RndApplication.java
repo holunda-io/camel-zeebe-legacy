b8cdef7833751283cbd0674d257ed519e9e2faff
Jan Galinski <jan.galinski@holisticon.de>
2018-01-19 12:57:39 +0100

    rnd example running

--- examples/random/src/main/java/io/zeebe/camel/examle/rnd/RndApplication.java
+++ examples/random/src/main/java/io/zeebe/camel/examle/rnd/RndApplication.java
@@ -1,9 +1,14 @@
 package io.zeebe.camel.examle.rnd;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.main.Main;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.camel.processor.aggregate.AggregationStrategy;
 
 @Slf4j
 public class RndApplication {
@@ -13,10 +18,34 @@ public class RndApplication {
         main.addRouteBuilder(new RouteBuilder() {
             @Override
             public void configure() throws Exception {
-                from("rnd:foo?generator=alphabetic&length=30")
-                    .to("stream:out");
+                from("rnd:foo?generator=alphanumeric&initialDelay=0&delay=50&length=100")
+                    .setHeader("foo", constant("bar"))
+                    .aggregate(header("foo"), new ListAggregationStrategy())
+                    .completionSize(5)
+                    .to("log:net.javaforge.blog.camel?level=INFO");
             }
         });
         main.run(args);
+    }
+
+    private static class ListAggregationStrategy implements AggregationStrategy
+    {
+
+        @SuppressWarnings("unchecked")
+        @Override
+        public Exchange aggregate(Exchange oldExchange, Exchange newExchange) {
+            Object newBody = newExchange.getIn().getBody();
+            List<Object> list;
+            if (oldExchange == null) {
+                list = new ArrayList<Object>();
+                list.add(newBody);
+                newExchange.getIn().setBody(list);
+                return newExchange;
+            } else {
+                list = oldExchange.getIn().getBody(List.class);
+                list.add(newBody);
+                return oldExchange;
+            }
+        }
     }
 }
